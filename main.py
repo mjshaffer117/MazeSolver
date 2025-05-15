245ef0b69d726eae7fc0819e3bfadd35ee4b9393
mjshaffer117 <mjshaffer117@gmail.com>
2025-05-14 23:54:41 -0400

    Added cell

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from window import *
+from cell import Cell
 
 def main():
     win = MainWindow(800, 600)
 
-    line = Line(Point(10, 100), Point(500, 100))
-    win.draw_line(line, "magenta")
+    cell = Cell(win)
+    cell.has_left_wall = False
+    cell.draw(50, 50, 100, 100)
 
-    line = Line(Point(10, 300), Point(500, 300))
-    win.draw_line(line, "cyan")
+    cell = Cell(win)
+    cell.has_bottom_wall = False
+    cell.draw(150, 150, 200, 200)
 
-    line = Line(Point(10, 500), Point(500, 500))
-    win.draw_line(line, "orange")
+    cell = Cell(win)
+    cell.has_right_wall = False
+    cell.draw(250, 250, 300, 300)
+
+    cell = Cell(win)
+    cell.has_top_wall = False
+    cell.draw(350, 350, 400, 400)
 
     win.wait_for_close()
 
