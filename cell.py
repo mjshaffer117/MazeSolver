1f95fcab5299b00d3b5b7af0fa04dc9da0bbd8d7
mjshaffer117 <mjshaffer117@gmail.com>
2025-05-16 22:57:51 -0400

    Created centered maze line functionality

--- cell.py
+++ cell.py
@@ -6,16 +6,18 @@ class Cell:
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-        self._x1 = None
-        self._x2 = None
-        self._y1 = None
-        self._y2 = None
+        self.x1 = 0
+        self.y1 = 0
+        self.x2 = 0
+        self.y2 = 0
         self._win = win
 
     def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
         self.x1 = x1
-        self.x2 = x2
         self.y1 = y1
+        self.x2 = x2
         self.y2 = y2
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
@@ -29,3 +31,15 @@ class Cell:
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        centerx1 = abs(self.x2 - self.x1) // 2 + self.x1
+        centery1 = abs(self.y2 - self.y1) // 2 + self.y1
+        centerx2 = abs(to_cell.x2 - to_cell.x1) // 2 + to_cell.x1
+        centery2 = abs(to_cell.y2 - to_cell.y1) // 2 + to_cell.y1
+        if undo is True:
+            fill_color = "dark blue"
+        else:
+            fill_color = "red"
+        line = Line(Point(centerx1, centery1), Point(centerx2, centery2))
+        self._win.draw_line(line, fill_color)
--- main.py
+++ main.py
@@ -4,21 +4,29 @@ from cell import Cell
 def main():
     win = MainWindow(800, 600)
 
-    cell = Cell(win)
-    cell.has_left_wall = False
-    cell.draw(50, 50, 100, 100)
+    cell_1 = Cell(win)
+    cell_1.has_right_wall = False
+    cell_1.draw(50, 50, 100, 100)
 
-    cell = Cell(win)
-    cell.has_bottom_wall = False
-    cell.draw(150, 150, 200, 200)
+    cell_2 = Cell(win)
+    cell_2.has_left_wall = False
+    cell_2.has_bottom_wall = False
+    cell_2.draw(100, 50, 150, 100)
 
-    cell = Cell(win)
-    cell.has_right_wall = False
-    cell.draw(250, 250, 300, 300)
+    cell_1.draw_move(cell_2)
 
-    cell = Cell(win)
-    cell.has_top_wall = False
-    cell.draw(350, 350, 400, 400)
+    cell_3 = Cell(win)
+    cell_3.has_top_wall = False
+    cell_3.has_right_wall = False
+    cell_3.draw(100, 100, 150, 150)
+
+    cell_2.draw_move(cell_3)
+
+    cell_4 = Cell(win)
+    cell_4.has_left_wall = False
+    cell_4.draw(150, 100, 200, 150)
+
+    cell_3.draw_move(cell_4, True)
 
     win.wait_for_close()
 
